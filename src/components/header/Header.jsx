9f3ee42f1569e0c333339688b6f9321fb0f9f317
Zofia Nora <sophienora92@gmail.com>
2025-09-09 12:53:00 +0200

    trying to add framer motion drag and drop animation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,10 @@
 import './Header.css'
+import * as motion from "motion/react-client"
+import { useRef } from "react"
 
 function Header() {
+    const constraintsRef = useRef(null)
+
     return(
         <>
         <div className="header">
@@ -8,7 +12,22 @@ function Header() {
                 <p className="header-name">Lyna Hutchi</p>
                 <p className="header-job">Designer & Creative Director</p>
             </div>
-            <div className="drag-container"></div>
+
+
+
+            <motion.div className="drag-container" ref={constraintsRef}>
+                <motion.div
+                drag
+                dragConstraints={constraintsRef}
+                dragElastic={0.2}
+                style={box}
+            />
+            </motion.div>
+
+
+
+
+
             {/* <div className="container-mail">lynahutchi@mail.com</div> */}
         </div>
         </>
@@ -16,3 +35,18 @@ function Header() {
 }
 
 export default Header;
+
+
+const constraints = {
+    width: 300,
+    height: 300,
+    backgroundColor: "var(--hue-1-transparent)",
+    borderRadius: 10,
+}
+
+const box = {
+    width: 100,
+    height: 100,
+    backgroundColor: "#ff0088",
+    borderRadius: 10,
+}
