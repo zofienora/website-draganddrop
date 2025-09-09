88dfff993b502781a50fec5522ab5cb957b7839b
Zofia Nora <sophienora92@gmail.com>
2025-09-09 12:47:01 +0200

    added name to header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
+import Header from './components/header/Header'
 
 function App() {
 
   return (
-    <>
-
+    <>  
+      <Header />        
     </>
   )
 }
 
-export default App
+export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,16 @@
+import './Header.css'
+
 function Header() {
     return(
         <>
+        <div className="header">
+            <div className="header-container">
+                <p className="header-name">Lyna Hutchi</p>
+                <p className="header-job">Designer & Creative Director</p>
+            </div>
+            <div className="drag-container"></div>
+            {/* <div className="container-mail">lynahutchi@mail.com</div> */}
+        </div>
         </>
     )
 }
