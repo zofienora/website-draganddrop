5b882010862a8a89ff272210d80799050c9757d1
Zofia Nora <sophienora92@gmail.com>
2025-09-17 19:14:40 +0200

    added about to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/header/Header'
+import About from './components/about/About'
 
 function App() {
 
   return (
     <>  
-      <Header />        
+      <Header />  
+      <About />      
     </>
   )
 }
