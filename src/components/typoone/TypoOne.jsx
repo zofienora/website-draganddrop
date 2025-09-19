c59c932df31be6a10a63274be87b7b60eaf53f22
Zofia Nora <sophienora92@gmail.com>
2025-09-19 16:16:35 +0200

    imported some framer motion

--- src/components/typoone/TypoOne.jsx
+++ src/components/typoone/TypoOne.jsx
@@ -1,3 +1,21 @@
+import { useRef } from "react";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  useMotionValue,
+  useVelocity,
+  useAnimationFrame
+} from "framer-motion";
+import { wrap } from "@motionone/utils";
+
+interface ParallaxProps {
+  children: string;
+  baseVelocity: number;
+}
+
+
 
 
 export default function TypoOne() {
