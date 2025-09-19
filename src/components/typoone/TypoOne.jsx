0065eafb47a058911145ee32402dd291ff1c7cea
Zofia Nora <sophienora92@gmail.com>
2025-09-19 16:14:14 +0200

    deleted border

---
+++ src/components/typoone/TypoOne.jsx
@@ -0,0 +1,13 @@
+
+
+export default function TypoOne() {
+    return(
+        <>
+        <div className="typoone">
+            <div className="container typoone-container">
+                <h1 className="typoone-text">Typo One</h1>
+            </div>
+        </div>
+        </>
+    )
+};
