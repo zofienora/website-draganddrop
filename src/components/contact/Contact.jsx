2a012f48d06a383ec960fd9389bcf72aefd30894
Zofia Nora <sophienora92@gmail.com>
2025-10-11 14:40:00 +0200

    added contact content

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,15 @@
+import "./Contact.css";
+
 function Contact() {
     return(
         <>
+        <div className="container-contact">
+            <h3 className="contact-header">Contact</h3>
+            <div className="contacts">
+                <p className="mail">lynahutchi@gmail.com</p>
+                <p className="insta">@lynahutchi</p>
+            </div>
+        </div>
         </>
     )
 }
